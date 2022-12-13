6223830d54dfa2b64f81ed09129afd272205998e
Marko Anic <73799942+anicmarko9@users.noreply.github.com>
2022-12-13 11:32:02 +0100

    Changed from POST to GET

--- Final/backend/routes/weather.route.ts
+++ Final/backend/routes/weather.route.ts
@@ -2,6 +2,6 @@ import * as express from "express";
 import { searchResults } from "../controllers/weather.controller";
 const router = express.Router();
 
-router.post("/", searchResults);
+router.get("/:cities/:countries", searchResults);
 
 export default router;
